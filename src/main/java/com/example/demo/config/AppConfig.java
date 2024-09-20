11216802127881ce79c34082dc63038b189ee005
Merl <phannakon.p@kkumail.com>
2024-09-20 22:11:18 +0700

    Update config to fix webclient(add loadbalance)

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -7,8 +7,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
+
     @Bean
-    @LoadBalanced
+    @LoadBalanced  // Ensures the lb:// scheme is recognized
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
     public WebClient webClient(WebClient.Builder builder) {
         return builder.baseUrl("lb://dorm-service/api/v1").build();
     }
